0e26045635004deffdac502ad40f812b0ee0335d
SARomanchuk <SARomanchuk@sberbank.ru>
2021-06-03 20:14:52 +0300

    les 3 hw

--- database/database.py
+++ database/database.py
@@ -17,11 +17,46 @@ class Database:
             instance = model(**data)
         return instance
 
+    def create_comments(self, post_id, data):
+        session = self.maker()
+        while True:
+            try:
+                comment = data.pop(0)
+            except IndexError:
+                break
+            author = self.get_or_create(
+                session,
+                models.Author,
+                "url",
+                dict(
+                    name=comment["comment"]["user"]["full_name"],
+                    url=comment["comment"]["user"]["url"],
+                    gb_id=comment["comment"]["user"]["id"],
+                ),
+            )
+            if not author.gb_id:
+                author.gb_id = comment["comment"]["user"]["id"]
+            comment_db = self.get_or_create(session, models.Comment, "id", comment["comment"],)
+            comment_db.author = author
+            comment_db.post_id = post_id
+            session.add(comment_db)
+            try:
+                session.commit()
+            except Exception:
+                session.rollback()
+            data.extend(comment["comment"]["children"])
+        session.close()
+
     def add_post(self, data):
         session = self.maker()
         post = self.get_or_create(session, models.Post, "id", data["post_data"])
-        author = self.get_or_create(session, models.Author, "url", data["author_data"])
-        post.author = author
+        post.author = self.get_or_create(session, models.Author, "url", data["author_data"])
+        post.tags.extend(
+            map(
+                lambda tag_data: self.get_or_create(session, models.Tag, "url", tag_data),
+                data["tags_data"],
+            )
+        )
         session.add(post)
         try:
             session.commit()
@@ -29,3 +64,4 @@ class Database:
             session.rollback()
         finally:
             session.close()
+        self.create_comments(data["post_data"]["id"], data["comments_data"])
