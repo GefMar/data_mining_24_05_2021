c1679bcb2edce0bc6cfae2037eb4ed2a664e6f9a
SARomanchuk <SARomanchuk@sberbank.ru>
2021-06-10 21:47:43 +0300

    les 6

--- gb_parse/pipelines.py
+++ gb_parse/pipelines.py
@@ -8,6 +8,8 @@
 from itemadapter import ItemAdapter
 from .settings import BOT_NAME
 from pymongo import MongoClient
+from scrapy import Request
+from scrapy.pipelines.images import ImagesPipeline
 
 
 class GbParsePipeline:
@@ -23,4 +25,15 @@ class GbMongoPipeline:
     def process_item(self, item, spider):
         collection_name = f"{spider.name}_{item.get('item_type', '')}"
         self.db[collection_name].insert_one(item)
+        return item
+
+
+class GbImageDownloadPipeline(ImagesPipeline):
+    def get_media_requests(self, item, info):
+        for url in item.get("photos", []):
+            yield Request(url)
+
+    def item_completed(self, results, item, info):
+        if results:
+            item["photos"] = [itm[1] for itm in results]
         return item
