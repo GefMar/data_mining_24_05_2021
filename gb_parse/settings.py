e823ecbb840671005a38d81d8afba1746a10751f
SARomanchuk <SARomanchuk@sberbank.ru>
2021-06-14 20:11:08 +0300

    les 6 HW

--- gb_parse/settings.py
+++ gb_parse/settings.py
@@ -7,7 +7,7 @@
 #     https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
 #     https://docs.scrapy.org/en/latest/topics/spider-middleware.html
 
-BOT_NAME = "gb_parse_18_05"
+BOT_NAME = "gb_parse_24_05"
 
 LOG_ENABLE = True
 LOG_LEVEL = "DEBUG"
@@ -22,12 +22,12 @@ USER_AGENT = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:88.0) Gecko/20100
 ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
-CONCURRENT_REQUESTS = 128
+CONCURRENT_REQUESTS = 64
 
 # Configure a delay for requests for the same website (default: 0)
 # See https://docs.scrapy.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-DOWNLOAD_DELAY = 0.02
+DOWNLOAD_DELAY = 2.5
 # The download delay setting will honor only one of:
 # CONCURRENT_REQUESTS_PER_DOMAIN = 16
 # CONCURRENT_REQUESTS_PER_IP = 16
@@ -52,10 +52,10 @@ DEFAULT_REQUEST_HEADERS = {
 
 # Enable or disable downloader middlewares
 # See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
-DOWNLOADER_MIDDLEWARES = {
-    "rotating_proxies.middlewares.RotatingProxyMiddleware": 610,
-    "rotating_proxies.middlewares.BanDetectionMiddleware": 620,
-}
+# DOWNLOADER_MIDDLEWARES = {
+#     "rotating_proxies.middlewares.RotatingProxyMiddleware": 610,
+#     "rotating_proxies.middlewares.BanDetectionMiddleware": 620,
+# }
 
 # Enable or disable extensions
 # See https://docs.scrapy.org/en/latest/topics/extensions.html
@@ -73,16 +73,16 @@ ITEM_PIPELINES = {
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/autothrottle.html
-# AUTOTHROTTLE_ENABLED = True
-# # # The initial download delay
-# AUTOTHROTTLE_START_DELAY = 2
-# # # The maximum download delay to be set in case of high latencies
-# AUTOTHROTTLE_MAX_DELAY = 3
-# # # The average number of requests Scrapy should be sending in parallel to
-# # # each remote server
-# AUTOTHROTTLE_TARGET_CONCURRENCY = 4.0
-# # # Enable showing throttling stats for every response received:
-# AUTOTHROTTLE_DEBUG = True
+AUTOTHROTTLE_ENABLED = True
+# # The initial download delay
+AUTOTHROTTLE_START_DELAY = 3
+# # The maximum download delay to be set in case of high latencies
+AUTOTHROTTLE_MAX_DELAY = 5
+# # The average number of requests Scrapy should be sending in parallel to
+# # each remote server
+AUTOTHROTTLE_TARGET_CONCURRENCY = 1.0
+# # Enable showing throttling stats for every response received:
+AUTOTHROTTLE_DEBUG = True
 
 # Enable and configure HTTP caching (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html#httpcache-middleware-settings
@@ -93,4 +93,4 @@ ITEM_PIPELINES = {
 # HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
 
 IMAGES_STORE = "images"
-ROTATING_PROXY_LIST_PATH = "proxies"
+# ROTATING_PROXY_LIST_PATH = "proxies"
